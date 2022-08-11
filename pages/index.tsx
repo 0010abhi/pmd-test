f2e4b7e5eb215b85187cb9d0e335b5bb634823ee
Abhishek Sachdeva <0010abhi@gmail.com>
2022-08-11 23:52:45 +0530

    graphql integrated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
 const Home: NextPage = () => {
+  const [data, setdata] = useState<any>([]);
+  useEffect(() => {
+    async function getData() {
+      const { data } = await client.query({
+        query: gql`
+          query {
+            firstPageArticles {
+              id
+              author
+              createdAt
+              score
+              updatedAt
+              title
+              text
+              type
+              url
+            }
+          }
+        `,
+      });
+      console.log("data", data);
+      setdata(data);
+    }
+    getData();
+    return () => {};
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +43,11 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {data &&
+            (data.firstPageArticles || []).map((datum: any, index: number) => (
+              <div key={index}>{datum.id}</div>
+            ))}
         </div>
       </main>
 
@@ -59,14 +57,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
